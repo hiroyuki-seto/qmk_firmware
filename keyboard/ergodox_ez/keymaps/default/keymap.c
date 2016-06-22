ddf243d532b7a8ea7f0b7257cd89dfb2e830df04
hiroyuki.seto <hiroyuki.seto@nohana.co.jp>
2016-06-22 14:44:28 +0900

    fix keymap

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_TRNS, KC_7,    KC_8,    KC_9,     KC_SLSH, KC_TRNS,
                 KC_TRNS, KC_4,    KC_5,    KC_6,     S(KC_QUOT), KC_TRNS,
        KC_TRNS, KC_TRNS, KC_1,    KC_2,    KC_3,     KC_MINUS,KC_TRNS,
-                KC_0,    KC_0,    KC_END,  KC_DOT,   S(KC_SCLN),
+                KC_0,    KC_0,    KC_DOT,  S(KC_SCLN),S(KC_MINS),
        KC_TRNS, KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_BTN1, KC_BTN2
